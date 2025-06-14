95c3f0efefeeda66d52cd92f7a5adb9d8235b6f5
vaibhav2396 <vaibhav.thapliyal6@gmail.com>
2025-06-14 15:00:15 -0400

    Initial Spring Boot project setup via Spring Initializer

---
+++ src/main/java/com/vtcode/recon/recon_engine/ReconEngineApplication.java
@@ -0,0 +1,13 @@
+package com.vtcode.recon.recon_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vtcode/recon/recon_engine/ReconEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtcode.recon.recon_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
